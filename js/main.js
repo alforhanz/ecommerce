f7479f3dcf33811e43930af2a27779b822ca7edf
Jose <alforhanz@gmail.com>
2024-10-19 19:27:58 -0500

    Iniciando el proyecto del E-Commerce creando index y carpetas del proyecto

---
+++ js/main.js
@@ -0,0 +1,5 @@
+  // Inicializar la barra lateral
+  document.addEventListener('DOMContentLoaded', function() {
+    let elems = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(elems);
+});
